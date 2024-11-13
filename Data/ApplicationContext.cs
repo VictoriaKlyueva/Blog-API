3d37634bf53acbd575a0ac6d0129b869295bb86d
vekosek <vika_klyueva@inbox.ru>
2024-11-13 16:22:05 +0700

    Packages installed

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using BackendLaboratory.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendLaboratory.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<LocalUser> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=usersdb;Username=postgres;Password=");
+        }
+    }
+}
