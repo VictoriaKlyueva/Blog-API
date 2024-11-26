f686c88d39dd30eaa496c527d1dcfdd9d132b024
Viktoriya Klyueva <vika_klyueva@inbox.ru>
2024-11-27 02:00:34 +0700

    Comment Models

---
+++ Data/DTO/CreateCommentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendLaboratory.Data.DTO
+{
+    public class CreateCommentDto
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(1000)]
+        public required string Content { get; set; }
+
+        public Guid? ParentId { get; set; }
+    }
+}
---
+++ Data/DTO/UpdateCommentDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendLaboratory.Data.DTO
+{
+    public class UpdateCommentDto
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(1000)]
+        public required string Content { get; set; }
+    }
+}
